655e8758c23bbae6d57823e1ce356e1d1eb96ff5
anandwana001 <akshaynandwana001@gmail.com>
2019-01-22 19:41:30 +0530

    Add 1 min progress countdown

--- app/src/main/java/com/akshay/circularprogresswithlist/MainActivity.kt
+++ app/src/main/java/com/akshay/circularprogresswithlist/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.akshay.circularprogresswithlist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setCountDownTimer(minutes = 1)
+    }
+
+    private fun setCountDownTimer(minutes: Int) {
+        progressBar.max = minutes * 60 * 1000
+        val mCountDownTimer = object : CountDownTimer((minutes * 60 * 1000).toLong(), 1) {
+            override fun onTick(millisUntilFinished: Long) {
+                progressBar.setProgress(millisUntilFinished.toInt())
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity, "Times Up", Toast.LENGTH_LONG).show()
+            }
+        }
+        mCountDownTimer.start()
     }
 }
